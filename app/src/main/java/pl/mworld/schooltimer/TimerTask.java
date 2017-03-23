ccba5a38bd71e4ed6d86fadd096bd020e2e71a6b
Oskar <themppspl@gmail.com>
2017-03-23 15:26:04 +0100

    Updated: Service into Async; Added: positioning timer in activity_main.xml

---
+++ app/src/main/java/pl/mworld/schooltimer/TimerTask.java
@@ -0,0 +1,24 @@
+package pl.mworld.schooltimer;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by ROOT on 2017-03-23.
+ */
+
+public class TimerTask extends AsyncTask {
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected Object doInBackground(Object[] params) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Object o) {
+        super.onPostExecute(o);
+    }
+}
