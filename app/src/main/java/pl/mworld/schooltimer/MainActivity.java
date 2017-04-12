1263a0b32a3fe78138e969a66b8447ef9502f17a
Oskar <themppspl@gmail.com>
2017-04-12 20:26:27 +0200

    Clean start in project

--- app/src/main/java/pl/mworld/schooltimer/MainActivity.java
+++ app/src/main/java/pl/mworld/schooltimer/MainActivity.java
@@ -1,38 +1,52 @@
 package pl.mworld.schooltimer;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-    static private TextView timerView;
-    private SharedPreferences mShared;
-
-    static public TextView getTimerView() {
-        return timerView;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        timerView = (TextView) findViewById(R.id.timerText);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        startService(new Intent(this, Timer.class));
+       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 
     @Override
-    protected void onStop() {
-        if (mShared.getBoolean("isNotificationOn", false) == true) {
-            stopService(new Intent(this, Timer.class));
-        }
-        super.onStop();
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
-    void floatingSettings(View view){
-        startActivity(new Intent(this, SettingsActivity.class));
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
