6e4766226caaec4b37989cf91b44da0024f416a4
Oskar <themppspl@gmail.com>
2017-05-06 23:49:15 +0200

    TimerService done

--- app/src/main/java/pl/mworld/schooltimer/TimerService.java
+++ app/src/main/java/pl/mworld/schooltimer/TimerService.java
@@ -2,9 +2,9 @@ package pl.mworld.schooltimer;
 
 import android.app.NotificationManager;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.AsyncTask;
 import android.os.CountDownTimer;
 import android.os.IBinder;
 import android.preference.PreferenceManager;
@@ -15,27 +15,35 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.List;
 
-//todo end this
 //todo extend -> stop notification and progress bar
 public class TimerService extends Service {
     NotificationCompat.Builder mBuilder;
     NotificationManager mManager;
+    Context context;
+    List<Long> filteredRingList;
     public TimerService() {
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-
         mManager.cancel(1);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
+        return START_STICKY;
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
+        context = getApplicationContext();
+
         SharedPreferences mShared = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         List<Long> ringlist = new ArrayList<>(20);
-        List<Long> filteredRingList = new ArrayList<>();
+        filteredRingList = new ArrayList<>();
         for(long l = 1; l < 21; l++) {
             ringlist.add(mShared.getLong(Long.toString(l), 0));
         }
@@ -43,7 +51,7 @@ public class TimerService extends Service {
             if (l != 0) filteredRingList.add(l);
         }
 
-        mBuilder = new NotificationCompat.Builder(getApplicationContext());
+        mBuilder = new NotificationCompat.Builder(context);
         mBuilder.setSmallIcon(R.drawable.ic_notify)
                 .setContentTitle(getString(R.string.remaining_time_to_ring))
                 .setContentText("")
@@ -52,6 +60,10 @@ public class TimerService extends Service {
                 .setWhen(0);
         mManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
+        runTimer();
+    }
+
+    void runTimer() {
         Calendar thatMoment = GregorianCalendar.getInstance();
         long sinceMidnight = thatMoment.get(Calendar.SECOND) +
                 (thatMoment.get(Calendar.MINUTE) * 60) + (thatMoment.get(Calendar.HOUR_OF_DAY) * 3600);
@@ -64,23 +76,9 @@ public class TimerService extends Service {
                     canRun = true;
                     break;
                 }
-                else {
-                    actualRing++;
-                    }
+                else actualRing++;
             }
-            else {
-//                new Thread(() -> {
-//                    mBuilder.setContentText("There will be no more rings today"); //todo make this string static
-//                    mManager.notify(1, mBuilder.build());
-//                    try {
-//                        wait(3000);
-//                    } catch (InterruptedException e) {
-//                        e.printStackTrace();
-//                    }
-//                    mManager.cancel(1);
-//                }).run();
-                break;
-            }
+            else break;
         }
 
         if(canRun) {
@@ -96,24 +94,10 @@ public class TimerService extends Service {
                 public void onFinish() {
                     mBuilder.setContentText(getString(R.string.ring_is_ringing));
                     mManager.notify(1, mBuilder.build());
-                    new AsyncTask<Void, Void, Void>() {
-                        @Override
-                        protected Void doInBackground(Void... params) {
-                            /*try {
-                                wait(1000);
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }*/
-                            //this.wait(3000);
-                            startService(new Intent(getApplicationContext(), TimerServiceRestarterService.class));
-                            return null;
-                        }
-                    }.execute();
+                    restart();
                 }
             }.start();
         }
-
-
     }
 
     String format(long l) {
@@ -129,6 +113,10 @@ public class TimerService extends Service {
         l = l - temp;
         s = s + String.valueOf(l) + "sec";//ss
         return s;
+    }
+
+    void restart() {
+        runTimer();
     }
 
     @Override
